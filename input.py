535d071925fa4bd2371dd23a314b76b8d6a3d06a
Wt666 <20431546@life.hkbu.edu.hk>
2021-05-31 15:25:28 +0800

    CC

--- input.py
+++ input.py
@@ -1,2 +1,8 @@
-a_input=input('Please give a number:')
-print('This input number is:',a_input)
+a_input=int(input('Please give a number:')) #return a string
+if a_input == 10:
+    print('This is a good one')
+elif a_input == 2:
+    print('See you next time')
+else:
+    print('Good luck')
+#print('This input number is:',a_input)
