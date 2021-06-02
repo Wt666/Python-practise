242f43e9d53e2aeafe4c3444c888893d44603368
Wt666 <20431546@life.hkbu.edu.hk>
2021-06-02 14:35:42 +0800

    matrix

--- numpy and pandas/3.py
+++ numpy and pandas/3.py
@@ -1,6 +1,18 @@
 import numpy as np
+from numpy.core.defchararray import count
 a=np.array([10,20,30,40])
 b=np.arange(4)
-c=a**b
+c=a**b #square
+d=10*np.sin(a)
+x=np.array([[1,2],[2,2]])
+y=b.reshape((2,2))
+z=x*y
+z_dot=np.dot(x,y) #matirx times
 print(a,b)
 print(c)
+print(d)
+print(d<5)
+print(x)
+print(y)
+print(z)
+print(z_dot)
