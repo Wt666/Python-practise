8397ac76b8a32295e97d28a4455b7eb3d8fddc5d
Wt666 <20431546@life.hkbu.edu.hk>
2021-06-16 18:20:21 +0800

    revise

--- matplotlib/5.py
+++ matplotlib/5.py
@@ -2,27 +2,28 @@ import matplotlib.pyplot as plt
 import numpy as np
 x=np.linspace(-3,3,50)
 y=2*x+1
-plt.figure(num=3,figsize=(8,5))
+plt.figure(num=3,figsize=(8,5)) #set the figure feature
 plt.plot(x,y,color='red',linewidth=1.0)
 new_ticks=np.linspace(-5,5,11)
 print(new_ticks)
 plt.yticks(new_ticks)
 #gca='get current axis'
 ax=plt.gca()
-ax.spines['right'].set_color('none')
-ax.spines['top'].set_color('none')
-ax.xaxis.set_ticks_position('bottom')
+ax.spines['right'].set_color('none') #make the right line disappear
+ax.spines['top'].set_color('none') #same as last line: top
+ax.xaxis.set_ticks_position('bottom') #default
 ax.yaxis.set_ticks_position('left')
 ax.spines['bottom'].set_position(('data',0)) #outward, axes
 ax.spines['left'].set_position(('data',0))
 x0=1
 y0=2*x0+1
-plt.scatter(x0,y0,s=50,color='g')
+plt.scatter(x0,y0,s=50,color='g') #choose the special point
 plt.plot([x0,x0],[y0,0],'k--',linewidth=2.5)
 #method 1
 #####
 plt.annotate(r'$2x+1=%s$'%y0,xy=(x0,y0),xycoords='data',xytext=(+30,-30),textcoords='offset points',fontsize=16,arrowprops=dict(arrowstyle='->',connectionstyle='arc3,rad=.2'))
 #method 2
 #####
+# r'$  $' :regular expression
 plt.text(-3.7,3,r'$This\ is\ the\ some\ text.\ \mu\ \sigma_i\ \alpha_t$',fontdict={'size':16,'color':'r'})
 plt.show()
--- numpy and pandas/p5.py
+++ numpy and pandas/p5.py
@@ -1,5 +1,10 @@
 import pandas as pd
 import numpy as np
+import pandas_profiling
 data=pd.read_csv('/Users/wt/Documents/Python-practise/numpy and pandas/student.csv')
 print(data)
+pandas_profiling.ProfileReport(data)
+pfr = pandas_profiling.ProfileReport(data)
+pfr.to_file('reportwt.html')
+print(data.describe())
 #data.to_pickle('student.pickle') #output
