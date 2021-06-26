7c1b6a98e86c54731a0cf2c31846294fbae6f814
Wt666 <20431546@life.hkbu.edu.hk>
2021-06-26 15:12:31 +0800

    6.26

---
+++ numpy and pandas/22.py
@@ -0,0 +1,48 @@
+print('I\'m apple')
+print("\"i like \"MOVIE\"\"")
+
+z=-1
+print(abs(z))
+print(pow(2,3))
+
+# n=int(input())
+# a=range(n)
+# for i in a:
+#     print(i,end="")
+
+a=[1,2,3,4]
+b=a
+a.append(4)
+print(a)
+print(b)
+x=15
+y=15
+z1=10
+print(x!=y)
+t=('a','b','c')
+print(3,)
+print(type(t))
+v=0.1
+print(isinstance(v,(int,float,str)))
+
+# pi=3.1415926
+# r=int(input("input your radius(must be integer):"))
+# s=pi*r**2
+# print(s)
+
+bill=30
+tip=bill*0.25
+total=bill+tip
+share=total*0.5
+print("Each person needs to pay:"+str(share))
+word1="Do"
+word2="you"
+word3="like"
+word4="Python?"
+print(word1+" "+word2+" "+word3+" "+word4)
+print("3+3="+str(3+3))
+
+numerator=30
+denominator=30
+result=numerator/denominator
+print(result)
