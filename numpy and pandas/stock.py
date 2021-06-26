aa2f697cceceab66e4e0ecda6dfa773c2c58fe90
Wt666 <20431546@life.hkbu.edu.hk>
2021-06-26 16:59:48 +0800

    6.26 afternoon

---
+++ numpy and pandas/stock.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import pandas_datareader
+import datetime as dt
+import pandas_datareader.data as web
+from pandas.plotting import scatter_matrix
+start=dt.datetime(2011,6,21)
+end=dt.datetime(2021,6,21)
+nvda=web.DataReader('NVDA','yahoo',start,end)
+print(nvda.tail(10))
+msft=web.DataReader('MSFT','yahoo',start,end) #6060.hk
+print(msft.head(10))
+nvda['Open'].plot(label='NVDA',figsize=(16,8),title='Open price',color='r')
+msft['Open'].plot(label='MSFT')
+plt.legend()
+tech_comp=pd.concat([nvda['Close'],msft['Close']],axis=1)
+tech_comp.columns=['NVDA Close','MSFT Close']
+scatter_matrix(tech_comp,figsize=(10,10),alpha=0.2,hist_kwds={'bins':50})
+plt.show()
